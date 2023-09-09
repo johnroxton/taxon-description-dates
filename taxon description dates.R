61e80b747ec1708d1b63807a3d3a9fad3a4904a4
psy02jpw <psy02jpw@bionb66.dom.uni-leipzig.de>
2023-09-09 19:26:02 +0200

    first files

---
+++ taxon description dates.R
@@ -0,0 +1,20 @@
+# TODO: Add comment
+# 
+# Author: David Schellenberger Costa
+###################################################################################################
+# 1
+###################################################################################################
+
+# 1################################################################################################
+nextScript <- NULL
+
+# load in libraries
+library(data.table)
+
+# clear workspace
+rm(list=ls())
+
+# set working directory
+setwd(paste0(.brd,"taxon description dates"))
+
+
